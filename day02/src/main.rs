379a20f2f5cf16409ead687d9b7d2c5e46bba383
panicbit <panicbit.dev@gmail.com>
2024-12-08 04:24:27 +0100

    day 02

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,6 +1,19 @@
 use aoc::aoc;
+use utils::tuple_split_parse;
 
 #[aoc(2015, 2, 1)]
 fn main(input: &str) -> i32 {
-    unimplemented!()
+    input
+        .lines()
+        .map(|line| tuple_split_parse::<i32, _>(line, "x"))
+        .map(|(l, w, h)| {
+            let a = l * w;
+            let b = w * h;
+            let c = h * l;
+
+            let smallest_side = a.min(b).min(c);
+
+            2 * a + 2 * b + 2 * c + smallest_side
+        })
+        .sum()
 }
