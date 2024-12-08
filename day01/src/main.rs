8a272c0f7ece852d2f137282458ffdb42cf236e3
panicbit <panicbit.dev@gmail.com>
2024-12-08 04:00:39 +0100

    day 01

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -2,5 +2,22 @@ use aoc::aoc;
 
 #[aoc(2015, 1, 1)]
 fn main(input: &str) -> i32 {
-    unimplemented!()
+    let directions = parse_directions(input);
+
+    visited_floors(directions).last().unwrap()
+}
+
+fn visited_floors(directions: impl Iterator<Item = i32>) -> impl Iterator<Item = i32> {
+    directions.scan(0, |floor, direction| {
+        *floor += direction;
+        Some(*floor)
+    })
+}
+
+fn parse_directions(input: &str) -> impl Iterator<Item = i32> + '_ {
+    input.chars().map(|ch| match ch {
+        '(' => 1,
+        ')' => -1,
+        _ => unreachable!(),
+    })
 }
